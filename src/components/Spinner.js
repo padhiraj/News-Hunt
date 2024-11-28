c92486ffa967d13c56ce375bdab023a92752738f
padhiraj <padhiraj5@gmail.com>
2024-11-28 14:41:09 +0530

    Add files via upload

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+ import React from 'react'
+ const Spinner =()=>{
+     return (
+       <div className='my-3'>
+        <div className="d-flex justify-content-center">
+  <div className="spinner-border" role="status">
+    <span className="sr-only"></span>
+  </div>
+</div>
+       </div>
+     )
+ }
+ export default Spinner
